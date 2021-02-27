67bffd998e177c32a84ed6d7dd34d62fc4c0c021
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-27 09:24:43 +0530

    Update songs.py

--- tg_bot/modules/songs.py
+++ tg_bot/modules/songs.py
@@ -269,9 +269,9 @@ async def download_video(v_url):
 
 
 __help__ = """
- • /song <song name>: uploads the song in it's best quality available
- • /video <video name>: uploads the video in it's best quality available
- • /lyrics <song name>: provides the lyrics of the song you want
+ - /song <song name>: uploads the song in it's best quality available
+ - /video <video name>: uploads the video in it's best quality available
+ - /lyrics <song name>: provides the lyrics of the song you want
 """
 
 __mod_name__ = "Songs 🎵"
